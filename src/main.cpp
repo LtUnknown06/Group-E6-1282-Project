a5c402f5c5a558452ba14e6dfd615076d3b5bf40
LtUnknown06 <nail.cameron1@gmail.com>
2026-03-06 13:28:30 -0500

    Commit 2
    hooray

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,38 @@
 #include <FEH.h>
+#include <FEHLCD.h> 
+#include <FEHIO.h> 
+#include <FEHUtility.h> 
+#include <FEHMotor.h> 
 #include <Arduino.h>
 
+//Declarations for encoders & motors 
 
-// Declare things like Motors, Servos, etc. here
-// For example:
-// FEHMotor leftMotor(FEHMotor::Motor0, 6.0);
-// FEHServo servo(FEHServo::Servo0);
+DigitalEncoder right_encoder(FEHIO::Pin10); 
+
+DigitalEncoder left_encoder(FEHIO::Pin8); 
+
+FEHMotor right_motor(FEHMotor::Motor0,9.0); 
+
+FEHMotor left_motor(FEHMotor::Motor3,9.0); 
+
+//Using encoders
+void milestone1_test1(int percent, float counts) { 
+    //Reset encoder counts 
+    right_encoder.ResetCounts(); 
+    left_encoder.ResetCounts(); 
+
+    //Set both motors to desired percent 
+    right_motor.SetPercent(percent); 
+    left_motor.SetPercent(percent); 
+
+    //While the average of the left and right encoder is less than counts, 
+    //keep running motors 
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts); 
+
+    //Turn off motors 
+    right_motor.Stop(); 
+    left_motor.Stop(); 
+} 
 
 
 void ERCMain()
