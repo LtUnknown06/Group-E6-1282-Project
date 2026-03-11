9a8be05a60e827b5642370767302a81ff2b21baf
LtUnknown06 <nail.cameron1@gmail.com>
2026-03-11 13:14:34 -0400

    e
    e

--- src/main.cpp
+++ src/main.cpp
@@ -5,41 +5,56 @@
 #include <FEHMotor.h> 
 #include <Arduino.h>
 
-//Declarations for encoders & motors 
+//Declarations for encoders & motors & CdS cell
 
-DigitalEncoder right_encoder(FEHIO::Pin10); 
+AnalogInputPin CdS(FEHIO::Pin1);
+
+/*DigitalEncoder right_encoder(FEHIO::Pin10); 
 
 DigitalEncoder left_encoder(FEHIO::Pin8); 
 
 FEHMotor right_motor(FEHMotor::Motor0,9.0); 
 
-FEHMotor left_motor(FEHMotor::Motor3,9.0); 
+FEHMotor left_motor(FEHMotor::Motor3,9.0); */
 
-//Using encoders
-void milestone1_test1(int percent, float counts) { 
-    //Reset encoder counts 
-    right_encoder.ResetCounts(); 
-    left_encoder.ResetCounts(); 
+//Start light read by CdS cell and final button activated
+void start() { 
+    //float variable x for CdS cell value and float variable base for comparison w/ x
+    float x = CdS.Value();
+    float base = 0.0;
 
-    //Set both motors to desired percent 
-    right_motor.SetPercent(percent); 
-    left_motor.SetPercent(percent); 
+    LCD.Write(base);
+    while (x > 0.5) {
+        x = CdS.Value();
+        LCD.Write(x);
+        Sleep(0.1);
+        LCD.Clear();
+    }
 
-    //While the average of the left and right encoder is less than counts, 
-    //keep running motors 
-    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts); 
+    //code to drive back and press final button, then turn to face straight toward ramp
 
-    //Turn off motors 
-    right_motor.Stop(); 
-    left_motor.Stop(); 
 } 
 
+void humidifier() { 
+    //float variable x for CdS cell value and float variable base for comparison w/ x
+    float x = CdS.Value();
+    float base = 0.0;
+
+    //while loop to read CdS cell value and print to LCD until value is less than base (indicating light is on)
+    LCD.Write(x);
+    while (x > base) {
+        x = CdS.Value();
+        LCD.Write(x);
+        Sleep(0.1);
+        LCD.Clear();
+    }
+
+    //if statements for different light colors/buttons so that robot moves to the right button
+
+} 
 
 void ERCMain()
 {
-    // Your code here!
-
-    // Or just use the TestGUI function
-    TestGUI();
-
+    start();
+    humidifier();
 }
